5e5bb69cb04ca3e2a359fe633bcdd809a3ca0e47
harukaxxxx <harukaxxxx@gmail.com>
2018-01-03 13:40:51 +0800

    fixing & cleaning

--- dashboard/assets/js/scripts.js
+++ dashboard/assets/js/scripts.js
@@ -1,80 +1,13 @@
 var query = location.search;
 switch (query) {
     case '?ships':
-        console.log('ships');
         var options = {
             valueNames: ['name', 'tier', 'id', 'type', 'nation']
         };
         new List('ships', options);
         break;
-    case '?caches':
-        console.log('caches');
-        var options = {
-            valueNames: ['no', 'filename', 'filecount', 'mdate']
-        };
-        new List('caches', options);
-        break;
-    case '?chart':
-        console.log('chart');
-        $.getJSON("database/records.json", function(json) {
-            var data = json['daily'];
-
-            var dateList = $.map(data, function(value, index) {
-                var y = index.substring(0, 4)
-                var m = index.substring(6, 4)
-                var d = index.substring(8, 6)
-                return [y + '/' + m + '/' + d];
-            });
-            var valueList = $.map(data, function(value, index) {
-                return [value];
-            });
-
-            var dailyDownloadChart = echarts.init(document.getElementById("daily"));
-            var maxValue = Math.max.apply(Math, valueList);
-
-            option = {
-
-                // Make gradient line here
-                visualMap: {
-                    show: false,
-                    type: 'continuous',
-                    seriesIndex: 0,
-                    min: 0,
-                    max: maxValue
-                },
-                title: {
-                    left: 'center',
-                    text: '每日下載量'
-                },
-                tooltip: {
-                    trigger: 'axis'
-                },
-                xAxis: {
-                    data: dateList
-                },
-                yAxis: {
-                    splitLine: { show: false }
-                },
-                dataZoom: [{
-                    startValue: '2017/09/04'
-                }, {
-                    type: 'inside'
-                }],
-                series: {
-                    name: '下載數',
-                    type: 'line',
-                    showSymbol: true,
-                    data: valueList
-                }
-            };
-
-            dailyDownloadChart.setOption(option);
-
-        });
-        break;
-    case '?database':
+    default:
         var mixer = mixitup('.container');
-        console.log('database');
 
         function submit(id) {
             var id = $(id)[0].id;
@@ -111,7 +44,7 @@ switch (query) {
                 "arp": arp
             };
             var band = ['艦隊收藏', '戰艦少女', '鋼鐵少女', '碧藍航線', 'November', '蒼藍鋼鐵戰艦', 'Victory Belles', '高校艦隊', '最終戰艦', '萌萌模式', '同人作品'];
-            band.forEach(function(band) {
+            band.forEach(function (band) {
                 var optionBand = $(select + " [name='" + band + "'] [name='band']").text();
                 if (optionBand) {
                     var c = $(select + " [name='" + band + "'] .am-input-group").length;
@@ -142,7 +75,7 @@ switch (query) {
                 }
             }, this);
 
-            $.getJSON("../database/" + sid + ".json", function(json) {
+            $.getJSON("../database/" + sid + ".json", function (json) {
 
                 var original = JSON.stringify(json, null, 2);
                 $('#json_original_' + sid).text(original);
@@ -152,10 +85,10 @@ switch (query) {
             $('#compare_' + sid).modal('open');
 
             //scroll synchronize
-            $('#json_original_' + sid).on('scroll', function() {
+            $('#json_original_' + sid).on('scroll', function () {
                 $('#json_edited_' + sid).scrollTop($(this).scrollTop());
             });
-            $('#json_edited_' + sid).on('scroll', function() {
+            $('#json_edited_' + sid).on('scroll', function () {
                 $('#json_original_' + sid).scrollTop($(this).scrollTop());
             });
         }
@@ -183,102 +116,6 @@ switch (query) {
             var jsonData = JSON.parse(jsonData);
             $.post("php/writedata.php", jsonData);
         }
-
-        break;
-    case '':
-        console.log('summary');
-        $.getJSON("database/complete.json", function(json) {
-            var nObj = Object.keys(json);
-            var doneData = [];
-            var nationData = [];
-
-            for (var i = 0; i < nObj.length; i++) {
-                var nation = nObj[i];
-                var data = json[nation];
-                var percent = Math.round(data[0] / data[1] * 100);
-                doneData.push(percent);
-
-                switch (nation) {
-                    case 'commonwealth':
-                        nation = "聯邦";
-                        break;
-                    case 'france':
-                        nation = "法國";
-                        break;
-                    case 'germany':
-                        nation = "德國";
-                        break;
-                    case 'italy':
-                        nation = "義大利";
-                        break;
-                    case 'japan':
-                        nation = "日本";
-                        break;
-                    case 'panasia':
-                        nation = "泛亞";
-                        break;
-                    case 'poland':
-                        nation = "波蘭";
-                        break;
-                    case 'uk':
-                        nation = "英國";
-                        break;
-                    case 'usa':
-                        nation = "美國";
-                        break;
-                    case 'ussr':
-                        nation = "蘇聯";
-                        break;
-                }
-                nationData.push(nation);
-
-            }
-
-            var nationCompleteChart = echarts.init(document.getElementById("nationComplete"));
-
-
-            option = {
-                tooltip: {
-                    trigger: 'axis',
-                    axisPointer: {
-                        type: 'shadow'
-                    }
-                },
-                grid: {
-                    left: '3%',
-                    right: '4%',
-                    bottom: '3%',
-                    top: '3%',
-                    containLabel: true
-                },
-                xAxis: {
-                    type: 'value',
-                    scale: true,
-                    min: 0,
-                    max: 100
-                },
-                yAxis: {
-                    type: 'category',
-                    data: nationData.reverse()
-                },
-                series: [{
-                    name: '完成數(%)',
-                    type: 'bar',
-                    label: {
-                        normal: {
-                            show: true,
-                            position: 'insideRight',
-                            formatter: '{c}%'
-                        }
-                    },
-                    data: doneData.reverse()
-                }]
-            };
-            nationCompleteChart.setOption(option);
-
-        });
-
-
 
         break;
 }
