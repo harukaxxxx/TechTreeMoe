01cf8d657e9cb922c0a59d534737a183d01cbd93
harukaxxxx <harukaxxxx@gmail.com>
2018-01-05 11:45:46 +0800

    tool links

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,100 +1,86 @@
 <?php
-    session_start();
-    include('database/session.php');
-    $page = $_SERVER['QUERY_STRING'];
-    $open_pages = array( 'ships');
-    switch ($page) {
-        case 'ships':
-            $title = "艦船列表";
-            break;
-        default:
-            $title = "資料庫";
-    }
+session_start();
+include 'database/session.php';
+$page = $_SERVER['QUERY_STRING'];
+$open_pages = array('ships');
+switch ($page) {
+    case 'ships':
+        $title = "艦船列表";
+        break;
+    default:
+        $title = "資料庫";
+}
 ?>
-<!DOCTYPE html>
-<html>
+    <!DOCTYPE html>
+    <html lang="zh-TW">
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title; ?></title>
-    <link rel="stylesheet" href="assets/css/amazeui.css" />
-    <link rel="stylesheet" href="assets/css/core.css" />
-    <link rel="stylesheet" href="assets/css/menu.css" />
-    <link rel="stylesheet" href="assets/css/index.css" />
-    <link rel="stylesheet" href="assets/css/admin.css" />
-    <link rel="stylesheet" href="assets/css/page/typography.css" />
-    <link rel="stylesheet" href="assets/css/page/form.css" />
-    <link rel="stylesheet" href="assets/css/component.css" />
-</head>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>
+            <?php echo $title; ?>
+        </title>
+        <link rel="stylesheet" href="assets/css/amazeui.css" />
+        <link rel="stylesheet" href="assets/css/core.css" />
+        <link rel="stylesheet" href="assets/css/menu.css" />
+        <link rel="stylesheet" href="assets/css/index.css" />
+        <link rel="stylesheet" href="assets/css/admin.css" />
+        <link rel="stylesheet" href="assets/css/page/typography.css" />
+        <link rel="stylesheet" href="assets/css/page/form.css" />
+        <link rel="stylesheet" href="assets/css/component.css" />
+    </head>
 
-<body>
-    <div class="admin">
-        <div class="admin-sidebar am-offcanvas  am-padding-0" id="admin-offcanvas">
-            <div class="am-offcanvas-bar admin-offcanvas-bar">
-                <div class="user-box am-hide-sm-only">
-                    <?php
-                        if(!isset($_SESSION['logged_in']) OR $_SESSION['logged_in'] != TRUE){
-                            ?>
-                            <div class="user-img">
-                        <img src="assets/img/anonymous.png" alt="user-img" title="Makino Haruka" class="img-circle img-thumbnail img-responsive">
-                    </div>
-                    <h5>Anonymous</h5>
-                            <?php
-                        } else {
-                            ?>
+    <body>
+        <div class="admin">
+            <div class="admin-sidebar am-offcanvas  am-padding-0" id="admin-offcanvas">
+                <div class="am-offcanvas-bar admin-offcanvas-bar">
+                    <div class="user-box am-hide-sm-only">
                         <div class="user-img">
-                        <img src="assets/img/avatar.png" alt="user-img" title="Makino Haruka" class="img-circle img-thumbnail img-responsive">
+                            <img src="assets/img/avatar.png" alt="user-img" title="Makino Haruka" class="img-circle img-thumbnail img-responsive">
+                        </div>
+                        <h5>Makino Haruka</h5>
                     </div>
-                    <h5 class="m-b-0">Makino Haruka</h5>
-                    <small><a href="./php/logout.php">登出</a></small>
-                            <?php
-                        }
-                    ?>
-                    <!-- <ul class="list-inline">
+                    <ul class="am-list admin-sidebar-list">
                         <li>
-                            <a href="#">
-                                <i class="fa fa-cog" aria-hidden="true"></i>
-                            </a>
+                            <a href="./">
+                                <span class="am-icon-home"></span> 首页</a>
                         </li>
-
                         <li>
-                            <a href="#" class="text-custom">
-                                <i class="fa fa-cog" aria-hidden="true"></i>
-                            </a>
+                            <a href="./?ships">
+                                <span class="am-icon-table"></span> 艦船列表</a>
                         </li>
-                    </ul> -->
+                        <li>
+                            <a href="php/copy_assistant.php">
+                                <span class="am-icon-terminal"></span> 原圖複製輔助</a>
+                        </li>
+                        <li>
+                            <a href="php/origin.php">
+                                <span class="am-icon-terminal"></span> 作業複製輔助</a>
+                        </li>
+                    </ul>
                 </div>
-                <ul class="am-list admin-sidebar-list">
-                    <li><a href="./"><span class="am-icon-home"></span> 首页</a></li>
-                    <li><a href="./?ships"><span class="am-icon-table"></span> 艦船列表</a></li>
-                </ul>
             </div>
-        </div>
-        <?php
-        if (in_array($page, $open_pages)) {
-            include("php/$page.php");
-        } else if($page == ''){
-            include('php/summary.php');
-        } else {
-            ?>
-            <h2>Page not found</h2>
-            <p>The page you requested was not found on this server.</p>
             <?php
-        }
-        ?>
+if (in_array($page, $open_pages)) {
+    include "php/$page.php";
+} else if ($page == '') {
+    include 'php/summary.php';
+}
+?>
+        </div>
+        </div>
 
-    </div>
-    </div>
+        <a href="admin-offcanvas" class="am-icon-btn am-icon-th-list am-show-sm-only admin-menu" data-am-offcanvas="{target: '#admin-offcanvas'}">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+        </a>
 
-    <a href="admin-offcanvas" class="am-icon-btn am-icon-th-list am-show-sm-only admin-menu" data-am-offcanvas="{target: '#admin-offcanvas'}"><i class="fa fa-bars" aria-hidden="true"></i></a>
+        <script type="text/javascript" src="assets/js/jquery-2.1.0.js"></script>
+        <script type="text/javascript" src="assets/js/amazeui.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
+        <script src="../assets/js/mixitup.min.js"></script>
+        <script type="text/javascript" src="assets/js/echarts.min.js"></script>
+        <script type="text/javascript" src="assets/js/scripts.js"></script>
+    </body>
 
-    <script type="text/javascript" src="assets/js/jquery-2.1.0.js"></script>
-    <script type="text/javascript" src="assets/js/amazeui.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
-    <script src="../assets/js/mixitup.min.js"></script>
-    <script type="text/javascript" src="assets/js/echarts.min.js"></script>
-    <script type="text/javascript" src="assets/js/scripts.js"></script>
-</body>
-
-</html>
+    </html>
