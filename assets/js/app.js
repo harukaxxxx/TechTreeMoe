370b82ff5a210c8f1262722ff2bdf53d7990556e
harukaxxxx <harukaxxxx@gmail.com>
2018-01-09 14:36:08 +0800

    download function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,67 +1,4 @@
-//ship box generater
-function generater() {
-    const nationList = ["commonwealth", "france", "germany", "italy", "japan", "panasia", "poland", "uk", "usa", "ussr"];
-    nationList.forEach(nation => {
-        $.get('assets/database/nationShips/' + nation + '.json')
-            .then((response) => {
-                $.each(response, function (index) {
-                    $.get('assets/database/shipJSON/' + index.toUpperCase() + '.json')
-                        .done(function (shipData) {
-
-                            //set box ID & data order
-                            const content = document.querySelector('#shipBox').content;
-                            content.querySelector('div').setAttribute('id', shipData['id'])
-                            content.querySelector('div').setAttribute('data-order', shipData['id'])
-
-                            //set box class
-                            if (shipData['premium']) { var premium = 'premium' } else { var premium = '' }
-                            if (shipData['change']) { var change = 'change' } else { var change = '' }
-                            let boxClassList = ['mix', shipData['type'], shipData['nation'], premium, change]
-                            content.querySelector('div').classList = boxClassList.join(' ')
-
-                            //set box title & tier icon
-                            content.querySelector('p').classList = 'ship icon-' + shipData['type']
-                            content.querySelector('p').innerHTML = '<span class="tier">' + shipData['tier'] + '</span>' + shipData['name']
-
-                            //set box image
-                            content.querySelector('img').src = 'assets/images/ship_previews/' + shipData['id'].toUpperCase() + '-' + shipData['default'] + '.png'
-
-                            //set box select
-                            content.querySelector('select').setAttribute('name', shipData['id'])
-                            content.querySelector('select').innerHTML = '<optgroup label="戰艦世界"><option value=' + shipData['id'] + '-0>【戰艦世界】' + shipData['name'] + '</option></optgroup>'
-
-                            // set box select options
-                            const bandList = ['艦隊收藏', '戰艦少女', '鋼鐵少女', '碧藍航線', '高校艦隊', '最終戰艦', '同人作品', 'November', '蒼藍鋼鐵戰艦', 'Victory Belles']
-                            const defaultOption = shipData['default']
-                            for (let bandIndex = 0; bandIndex < bandList.length; bandIndex++) {
-                                const band = bandList[bandIndex];
-                                if (shipData[band]) {
-                                    let optgroup = ['<optgroup label="' + band + '">']
-                                    $.each(shipData[band], function (optID, optName) {
-                                        if (defaultOption == optID) { var selected = ' selected' } else { var selected = '' }
-                                        optgroup.push('<option value=' + shipData['id'] + '-' + optID + selected + '>' + optName + '</option>')
-                                    });
-                                    optgroup.push('</optgroup>')
-                                    content.querySelector('select').innerHTML += optgroup.join('')
-                                }
-                            }
-
-                            //append ship box
-                            document.querySelector('#Container').appendChild(
-                                document.importNode(content, true)
-                            );
-
-                            // generate select 
-                            $('#' + shipData['id'] + ' select').selected('enable')
-
-                        })
-                })
-
-            })
-    })
-}
-
-//change img whem optio
+//change img when option changed
 $('.mixitup select').on('change', function () {
     var ship = this.name
     var ship_img = this.value
@@ -78,34 +15,39 @@ if (!store.enabled) {
     alert('您的瀏覽器不支援本地儲存功能，儲存紀錄功能將不可用！')
 }
 
-//get selected
-var img_list = []
-
+//download function
 function submit() {
-    //progress bar start
+    //progress start
     $.AMUI.progress.start()
+    $('#submit').button('loading')
 
-    //generate file name list
-    var selectedList = $('.mixitup select')
-    var c = selectedList.length
-    for (var i = 0; i < c; i++) {
-        var value = selectedList[i].value
-        if (value.substring(8) != 0) {
-            img_list.push(value)
+    //generate zip structure
+    let zip = new JSZip();
+    let ship_previews = zip.folder('gui/ship_previews')
+    let ship_previews_ds = zip.folder('gui/ship_previews_ds')
+
+    // packing files to zip
+    let selectedList = $('.mixitup select')
+    for (let i = 0; i < selectedList.length; i++) {
+        const imageID = selectedList[i].value
+        if (imageID.substring(8) != 0) {
+            let imageFile = $.get('assets/images/ship_previews/' + imageID + '.png')
+            ship_previews.file(imageID.substring(0, 7) + '.png', imageFile)
+            let imageFileDS = $.get('assets/images/ship_previews/' + imageID + '.png')
+            ship_previews_ds.file(imageID.substring(0, 7) + '.png', imageFileDS)
         }
     }
-    console.log(img_list)
-    $.AMUI.progress.done()
-}
 
-// download button
-$('#submit').click(function () {
-    var $btn = $(this)
-    $btn.button('loading')
-    setTimeout(function () {
-        $btn.button('reset')
-    }, 5000)
-})
+    // download zip
+    zip.generateAsync({ type: "blob" })
+        .then(function (content) {
+            saveAs(content, "res_mod.zip");
+
+            //progress done
+            $.AMUI.progress.done()
+            $('#submit').button('reset')
+        });
+}
 
 // mixItUp
 var mixer = mixitup('#Container', {
