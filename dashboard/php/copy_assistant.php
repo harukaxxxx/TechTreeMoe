773e953ac1b32b817a64e57ad01ef5378733467e
harukaxxxx <harukaxxxx@gmail.com>
2018-02-01 15:30:23 +0800

    prevent rename png to jpg

--- dashboard/php/copy_assistant.php
+++ dashboard/php/copy_assistant.php
@@ -5,12 +5,11 @@ $list = scandir($dirTarget);
 
 foreach ($list as $filename) {
     if (substr($filename,0,1) == "-") {
-       $shipname = substr($filename,1,7);
-        $editname = substr($filename,1);
-
-        if (!file_exists("$dirTarget/$ship/$editname")) {
+        $shipname = substr($filename,1,7);
+        $editname = substr($filename,1,9);
+        if (!file_exists("$dirTarget/$ship/$editname.png")) {
             echo "Copying $shipname <br>";
-            copy("$dirOrigin/$shipname.png","$dirTarget/$ship/$editname");
+            copy("$dirOrigin/$shipname.png","$dirTarget/$ship/$editname.png");
         }else {
             echo "$shipname exists!!<br>";
         }  
