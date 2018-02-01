6e8fcc05db39cb06f32acec0b9a825b57407d50d
harukaxxxx <harukaxxxx@gmail.com>
2018-02-01 15:31:41 +0800

    bug fix

--- dashboard/php/copy_assistant.php
+++ dashboard/php/copy_assistant.php
@@ -6,7 +6,7 @@ $list = scandir($dirTarget);
 foreach ($list as $filename) {
     if (substr($filename,0,1) == "-") {
         $shipname = substr($filename,1,7);
-        $editname = substr($filename,1,9);
+        $editname = substr($filename,1,-4);
         if (!file_exists("$dirTarget/$ship/$editname.png")) {
             echo "Copying $shipname <br>";
             copy("$dirOrigin/$shipname.png","$dirTarget/$ship/$editname.png");
