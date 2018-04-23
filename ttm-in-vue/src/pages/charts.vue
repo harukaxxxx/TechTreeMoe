7710ad041c67eb882b661942a0cc5bed87f87ace
harukaxxxx <harukaxxxx@gmail.com>
2018-04-23 16:42:35 +0800

    i18n with 4 langs

--- ttm-in-vue/src/pages/charts.vue
+++ ttm-in-vue/src/pages/charts.vue
@@ -111,7 +111,7 @@ export default {
       chartSettings: {
         area: true,
         labelMap: {
-          value: this.$t('charts.lineChartLabel')
+          value: 'Downloaded'
         },
         label: {
           normal: {
