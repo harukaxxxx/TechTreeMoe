b605d0f0f7ebd471a6e34cb05cd65d0afbedd472
MakinoHaruka <harukaxxxx@gmail.com>
2018-07-12 10:01:58 +0800

    router path setup

--- vuecli3/src/router.js
+++ vuecli3/src/router.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Intro from './views/Intro.vue'
+import Preview from './views/Preview.vue'
+import Custom from './views/Custom.vue'
 import About from './views/About.vue'
-
+import Charts from './views/Charts.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -13,9 +16,29 @@ export default new Router({
       component: Home
     },
     {
+      path: '/intro',
+      name: 'intro',
+      component: Intro
+    },
+    {
+      path: '/preview',
+      name: 'preview',
+      component: Preview
+    },
+    {
+      path: '/custom',
+      name: 'custom',
+      component: Custom
+    },
+    {
       path: '/about',
       name: 'about',
       component: About
+    },
+    {
+      path: '/charts',
+      name: 'charts',
+      component: Charts
     }
   ]
 })
