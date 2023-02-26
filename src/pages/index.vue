2d15ec684d4b7f8667635c7e539ee666fa42e8fe
MakinoHaruka <harukaxxxx@makinoworks.com>
2023-02-27 01:50:03 +0800

    Install pinia

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from "@/components/TheWelcome.vue";
+
+import { useCounterStore } from "@/stores/counter.js"
+
+const store = useCounterStore()
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <h1>{{ store.counter }}</h1>
+    <button @click="store.increment">Click</button>
   </main>
 </template>
