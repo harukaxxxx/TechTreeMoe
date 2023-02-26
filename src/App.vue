1997e6fc469224ddf9fa7c5711e0765ab3b479fb
MakinoHaruka <harukaxxxx@makinoworks.com>
2023-02-27 00:38:54 +0800

    Init Vue3

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,85 @@
-<template>
-  <div id="app">
-    <Layout>
-      <Header :style="{background:navBackground}">
-        <app-header />
-      </Header>
-      <Content>
-        <router-view></router-view>
-      </Content>
-      <Footer>
-        <app-footer />
-      </Footer>
-    </Layout>
-  </div>
-</template>
-<script>
-import iconfont from '@/iconfont.js'
-import appHeader from '@/components/navbar.vue'
-import appFooter from '@/components/footer.vue'
-
-export default {
-  name: 'app',
-  components: {
-    appHeader,
-    appFooter,
-    iconfont
-  },
-  computed: {
-    navBackground: function() {
-      switch (this.$route.name) {
-        case 'home':
-        case 'charts':
-          return '#1e5b94'
-        case 'intro':
-        case 'custom':
-        case 'about':
-          return '#fff'
-      }
-    }
-  }
-}
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
-<style>
-.icon {
-  width: 1em;
-  height: 1em;
-  vertical-align: -0.15em;
-  fill: currentColor;
-  overflow: hidden;
-}
-</style>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
 
 <style scoped>
-.ivu-layout-header {
-  padding: 0;
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
-.ivu-layout-footer {
-  padding: 0;
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,29 +1,14 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
-import store from './store'
-import i18n from './i18n'
-import iView from 'iview'
-import 'iview/dist/styles/iview.css'
-Vue.use(iView)
 
-const isProd = process.env.NODE_ENV === 'production'
-import VueAnalytics from 'vue-analytics'
-Vue.use(VueAnalytics, {
-  id: 'UA-42562282-2',
-  router,
-  debug: !isProd,
-  sendHitTask: isProd,
-  autoTracking: {
-    page: isProd
-  }
-})
+import './assets/main.css'
 
-Vue.config.productionTip = false
+const app = createApp(App)
 
-new Vue({
-  router,
-  store,
-  i18n,
-  render: h => h(App)
-}).$mount('#app')
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
