6d3f368c69ee51621458c5c2b2da51f5dcc031a2
MakinoHaruka <harukaxxxx@makinoworks.com>
2023-02-27 00:45:32 +0800

    install unplugin-auto-import

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,17 @@ import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import AutoImport from "unplugin-auto-import/vite";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ["vue", "vue-router", "vue-i18n"],
+      dts: "src/auto-imports.js",
+    }),
+  ],
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`,
